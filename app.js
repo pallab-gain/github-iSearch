39bcd277e85b706c005124c41d495c88745f7fd2
pallab-gain <pallab.gain@gmail.com>
2014-07-15 21:31:16 +0600

    remove custom slowdown

--- app.js
+++ app.js
@@ -12,41 +12,6 @@ app.factory('searchRepo', function ($resource) {
     return searchRepo;
 });
 
-app.factory('slowDown', function () {
-    var slowDown = {};
-    slowDown.pressed = false;
-    slowDown.interval = (0.3 * 1000);
-    slowDown.curtime = undefined;
-
-
-    slowDown.resolve = function (query_string, callback) {
-        if (slowDown.pressed === false) {
-            slowDown.pressed = true;
-            slowDown.query = query_string;
-            slowDown.curtime = new Date().getTime();
-            return callback({
-                'str': query_string,
-                'ok': true
-            });
-        } else {
-            var next_time = new Date().getTime();
-            if (next_time > slowDown.curtime + slowDown.interval && slowDown.query !== query_string) {
-                slowDown.query = query_string;
-                slowDown.curtime = next_time;
-                return callback({
-                    'str': query_string,
-                    'ok': true
-                });
-            } else {
-                return callback({
-                    'str': query_string,
-                    'ok': false
-                });
-            }
-        }
-    };
-    return slowDown;
-});
 app.controller('githubInstance', function ($scope, searchRepo) {
     $scope.searchToken = undefined;
     $scope.fetch_links = function () {
